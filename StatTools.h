b9801798485be03463a063ea49e858170688a7ac
Jay <jay@Jays-MacBook-Air.local>
2024-07-21 21:02:37 -0500

    Fin: Finished (temporariliy) StatTools

--- StatTools.h
+++ StatTools.h
@@ -1,5 +1,13 @@
 #pragma once
 
-class StatTools{
-
+class StatTools
+{
+public:
+    static long long max(const long long numbers[], int size);
+    static long long min(const long long numbers[], int size);
+    static double mean(const long long numbers[], int size);
+    static long long mode(const long long numbers[], int size);
+    static double median(long long numbers[], int size);
+    static void stemAndLeafPlot(const long long numbers[], int size);
+    static void barChart(const long long numbers[], int size);
 };
